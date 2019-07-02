9f0e65652991d003df72c48f7568a475d4408d6f
Elena <bunina.elena07@gmail.com>
2019-07-02 23:05:45 +0300

    обработчик событий прописан в коде, и подсказка стала всплывающей

--- app/src/main/java/com/example/testproject/MainActivity.java
+++ app/src/main/java/com/example/testproject/MainActivity.java
@@ -1,27 +1,54 @@
 package com.example.testproject;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.os.Build;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText text_from_user;
+    private TextView result;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        onButtonClick();
     }
-    public void onButtonClick (View view) {
-        EditText text_from_user = (EditText) findViewById(R.id.editText);
-        TextView result = (TextView) findViewById(R.id.result_filed);
-        String text = text_from_user.getText().toString();
-        if (!text.equals(""))
-        {
-            float num = Float.parseFloat(text);
-            num *= 1.61;
-            result.setText(Float.toString(num));
-        } else result.setText("Введите число");
+
+    public void onButtonClick() {
+        text_from_user = findViewById(R.id.editText);
+        result = findViewById(R.id.result_filed);
+        button = findViewById(R.id.button_convert);
+
+        //обработчик события - нажатие кнопки button = button_convert
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = text_from_user.getText().toString();
+                if (!text.equals("")) {
+                    float num = Float.parseFloat(text);
+                    num *= 1.61;
+                    result.setText(Float.toString(num));
+                    button.setText("Готово");
+                    //проверка позволяет ли версия изменить цвет кнопки c tint
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                        button.setBackgroundTintList(ColorStateList.valueOf(Color.GREEN));
+                    } else button.setBackgroundColor(Color.GREEN);
+                    //всплывающая подсказка
+                } else Toast.makeText(MainActivity.this,
+                        "Введите число",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
